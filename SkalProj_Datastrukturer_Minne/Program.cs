533efdef479caf1cd18edba0e20afdb5cd844def
98emre <emre-98@live.se>
2024-03-19 01:44:41 +0100

    feat: assignment 4 done

--- SkalProj_Datastrukturer_Minne/Program.cs
+++ SkalProj_Datastrukturer_Minne/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace SkalProj_Datastrukturer_Minne
 {
@@ -60,6 +61,7 @@ namespace SkalProj_Datastrukturer_Minne
         /// <summary>
         /// Examines the datastructure List
         /// </summary>
+
         static void ExamineList()
         {
             /*
@@ -72,12 +74,81 @@ namespace SkalProj_Datastrukturer_Minne
              * Below you can see some inspirational code to begin working.
             */
 
-            //List<string> theList = new List<string>();
-            //string input = Console.ReadLine();
-            //char nav = input[0];
-            //string value = input.substring(1);
+            List<string> list = new List<string>();
+            bool running = true;
 
-            //switch(nav){...}
+            while (running)
+            {
+
+                try
+                {
+                    Console.WriteLine("Use (+) for adding, (-) is for remove and (0) to exit ");
+
+                    string input = Console.ReadLine()!;
+                    char nav = input[0];
+                    string value = input.Substring(1).Trim();
+
+
+                    switch (nav)
+                    {
+                        case '+':
+
+                            if (string.IsNullOrEmpty(value))
+                            {
+                                throw new ArgumentException("The input was invalid");
+                            }
+
+                            list.Add(value);
+                            Console.WriteLine($"Capacity: {list.Capacity}, Count: {list.Count}");
+                            break;
+
+                        case '-':
+                            if (list.Count == 0)
+                            {
+                                throw new ArgumentException("The list is empty");
+                            }
+
+                            list.Remove(value);
+                            Console.WriteLine($"Capacity: {list.Capacity}, Count: {list.Count}");
+                            break;
+
+
+                        case '0':
+                            Console.WriteLine("Existing the list session");
+                            running = false;
+                            break;
+
+                        default:
+                            Console.WriteLine("Invalid input");
+                            break;
+                    }
+                }
+
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message.ToString());
+                }
+            }
+        }
+
+
+        private static string PrintTheQueue(Queue<string> queue)
+        {
+            if (queue.Count == 0)
+            {
+                return "Empty";
+            }
+
+            StringBuilder sb = new StringBuilder();
+
+            foreach (string item in queue)
+            {
+                sb.Append(item);
+                sb.Append(", ");
+            }
+
+            sb.Remove(sb.Length - 2, 2); // Ta bort sista kommatecknet och tomma ytan
+            return sb.ToString();
         }
 
         /// <summary>
@@ -90,6 +161,78 @@ namespace SkalProj_Datastrukturer_Minne
              * Create a switch with cases to enqueue items or dequeue items
              * Make sure to look at the queue after Enqueueing and Dequeueing to see how it behaves
             */
+
+            Queue<string> queue = new Queue<string>();
+            bool running = true;
+
+            while (running)
+            {
+                try
+                {
+                    Console.WriteLine("\n0. Exit \n1. Enqueue\n2. Dequeue item");
+
+                    Console.Write("Enter a number: ");
+                    string input = Console.ReadLine()!.Trim();
+
+                    switch (input)
+                    {
+                        case "0":
+                            Console.WriteLine("Existing queue session");
+                            running = false;
+                            break;
+
+                        case "1":
+                            Console.Write("Enter a person: ");
+                            string item = Console.ReadLine()!;
+
+                            if (string.IsNullOrEmpty(item) || item.Trim().Length <= 0)
+                            {
+                                throw new ArgumentException("Entering was not valid");
+                            }
+
+                            queue.Enqueue(item);
+                            Console.WriteLine($"{item} got enqueue.");
+                            Console.WriteLine($"Current queue: {PrintTheQueue(queue)}");
+                            break;
+
+                        case "2":
+                            string dequeueItem = queue.Dequeue();
+                            Console.WriteLine($"\n{dequeueItem} got dequeue.");
+                            Console.WriteLine($"Current queue: {PrintTheQueue(queue)}");
+                            break;
+
+                        default:
+                            Console.WriteLine("Invalid input choose a number between 0-2");
+                            break;
+                    }
+                }
+
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message.ToString());
+                }
+            }
+        }
+
+
+        static string PrintTheStack( Stack<string> stack )
+        {
+            if(stack.Count <= 0)
+            {
+                return "Empty";
+            }
+
+            StringBuilder sb = new StringBuilder();
+
+            foreach( string item in stack)
+            {
+                sb.Append(item);
+                sb.Append(", ");
+            }
+
+            sb.Remove(sb.Length - 2, 2);
+
+            return sb.ToString();
         }
 
         /// <summary>
@@ -102,7 +245,68 @@ namespace SkalProj_Datastrukturer_Minne
              * Create a switch with cases to push or pop items
              * Make sure to look at the stack after pushing and and poping to see how it behaves
             */
+
+
+            Stack<string> stack = new Stack<string>();
+            bool running = true;
+
+
+            while (running)
+            {
+                try
+                {
+                    Console.WriteLine("\n0.Exit the stack \n1.Push \n2.Pop");
+                    Console.Write("Choose a number: ");
+                    string input = Console.ReadLine()!;
+
+                    switch (input) 
+                    {
+                        case "0":
+                            Console.WriteLine("Exit the stack system");
+                            running = false;
+                            break;
+
+                        case "1":
+                            Console.Write("Enter what you want to push: ");
+                            string item = Console.ReadLine()!;
+
+
+                            if(string.IsNullOrEmpty(item) || item.Trim().Length <= 0)
+                            {
+                                throw new ArgumentException("Entering was invalid");
+                            }
+
+                            string reversed = new string(item.Reverse().ToArray());
+                            stack.Push(reversed);
+
+                            Console.WriteLine($"You entered {item}, which was reversed to {reversed} and then got pushed to ");
+                            break;
+
+                        case "2":
+                            if(stack.Count == 0)
+                            {
+                                throw new ArgumentException("The stack is empty");
+                            }
+
+                            string itemPop = stack.Pop();
+                            Console.WriteLine($"Item got pop is {itemPop}");
+                            Console.WriteLine($"Current Stack: {PrintTheStack(stack)}");
+                            break;
+
+                        default:
+                            Console.WriteLine("Invalid input, try again and choose a number between 0-2");
+                            break;
+                    }
+                }
+
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message.ToString());
+                }
+            }
+
         }
+
 
         static void CheckParanthesis()
         {
@@ -112,8 +316,40 @@ namespace SkalProj_Datastrukturer_Minne
              * Example of incorrect: (()]), [), {[()}],  List<int> list = new List<int>() { 1, 2, 3, 4 );
              */
 
-        }
+            Stack<char> stack = new Stack<char>();
 
+            try
+            {
+                Console.Write("What do you want to enter: ");
+                string input = Console.ReadLine()!;
+
+                if (string.IsNullOrWhiteSpace(input) || input.Trim().Length == 0)
+                {
+                    throw new ArgumentException("You cannot enter an empty value.");
+                }
+
+                foreach (char c in input)
+                {
+                    if (c == '(' || c == '[' || c == '{')
+                    {
+                        stack.Push(c);
+                    }
+                    else if (c == ')' || c == ']' || c == '}')
+                    {
+                        if ((c == ')' && stack.Pop() != '(') || (c == ']' && stack.Pop() != '[') || (c == '}' && stack.Pop() != '{'))
+                        {
+                            throw new ArgumentException("It is not well-formed");
+                        }
+                    }
+                }
+
+                Console.WriteLine(stack.Count == 0 ? "It is well-formed." : "It is not well-formeds");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
     }
 }
 
